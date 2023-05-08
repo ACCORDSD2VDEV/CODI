d5047ce2c0935493b2124a37d75ff6800eda09ec
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-05-08 15:29:03 -0600

    Update cohort_demographic_age_filter.sql
    See note on changes to cohort.sql for more details. Modifications made to this code:
    
    •	Lines 21-23: Change 2017, 2018, 2019 to SY1, SY2, SY3, respectively
    •	Lines 21-23: Modify WHERE clause to include SY4, SY5, SY6

--- 2.1_Partner/sql/Step1/CODIVDW/cohort_demographic_age_filter.sql
+++ 2.1_Partner/sql/Step1/CODIVDW/cohort_demographic_age_filter.sql
@@ -18,7 +18,10 @@ FROM (
   FROM 
   	#cohort_demographic_age 
   WHERE 
-  	(yr = 2017 AND study_age_yrs_2017 BETWEEN 2 AND 19) OR
-  	(yr = 2018 AND study_age_yrs_2018 BETWEEN 2 AND 19) OR
-  	(yr = 2019 AND study_age_yrs_2019 BETWEEN 2 AND 19)
+  	(yr = SY1 AND study_age_yrs_2017 BETWEEN 2 AND 19) OR
+  	(yr = SY2 AND study_age_yrs_2018 BETWEEN 2 AND 19) OR
+  	(yr = SY3 AND study_age_yrs_2019 BETWEEN 2 AND 19) OR 
+    (yr = SY4 AND study_age_yrs_2019 BETWEEN 2 AND 19) OR 
+    (yr = SY5 AND study_age_yrs_2019 BETWEEN 2 AND 19) OR 
+    (yr = SY6 AND study_age_yrs_2019 BETWEEN 2 AND 19) 
 ) a;
