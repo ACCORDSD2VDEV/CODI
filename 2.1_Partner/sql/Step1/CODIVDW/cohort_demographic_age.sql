52a767902ee3a07603351783b04e843337aaf16f
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-05-08 15:08:27 -0600

    Update cohort_demographic_age.sql
    See note for change on commit to cohort.sql for more detail. Changes described below: 
    
    cohort_demographic_age.sql
    •	Lines 6-20: Change ‘1/1/2017’ to ‘6/1/2017’, ‘1/1/2018’ to ‘6/1/2018’,  & ‘1/1/2019’ to ‘6/1/2019’
    •	Lines 10, 15, 20: Change 2017, 2018, 2019 to SY1, SY2, SY3, respectively 
    •	Add similar case statements for study_age_years_SY0, study_age_years_SY4, study_age_years_SY5, study_age_years_SY6 
    •	Lines 6-20: Change ‘1/1/2017’ to ‘6/1/2017’, ‘1/1/2018’ to ‘6/1/2018’,  & ‘1/1/2019’ to ‘6/1/2019’

--- 2.1_Partner/sql/Step1/CODIVDW/cohort_demographic_age.sql
+++ 2.1_Partner/sql/Step1/CODIVDW/cohort_demographic_age.sql
@@ -4,19 +4,39 @@ INTO #cohort_demographic_age
 FROM (
 	SELECT *
 		,CASE 
-			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2017'), birth_date), '1/1/2017') < 0
-				THEN DATEDIFF(YEAR, birth_date, '1/1/2017') - 1
-			ELSE DATEDIFF(YEAR, birth_date, '1/1/2017')
-			END AS study_age_yrs_2017
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '6/1/2016'), birth_date), '6/1/2016') < 0
+				THEN DATEDIFF(YEAR, birth_date, '6/1/2016') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '6/1/2016')
+			END AS study_age_yrs_SY0
 		,CASE 
-			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2018'), birth_date), '1/1/2018') < 0
-				THEN DATEDIFF(YEAR, birth_date, '1/1/2018') - 1
-			ELSE DATEDIFF(YEAR, birth_date, '1/1/2018')
-			END AS study_age_yrs_2018
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '6/1/2017'), birth_date), '6/1/2017') < 0
+				THEN DATEDIFF(YEAR, birth_date, '6/1/2017') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '6/1/2017')
+			END AS study_age_yrs_SY1
 		,CASE 
-			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2019'), birth_date), '1/1/2019') < 0
-				THEN DATEDIFF(YEAR, birth_date, '1/1/2019') - 1
-			ELSE DATEDIFF(YEAR, birth_date, '1/1/2019')
-			END AS study_age_yrs_2019
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '6/1/2018'), birth_date), '6/1/2018') < 0
+				THEN DATEDIFF(YEAR, birth_date, '6/1/2018') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '6/1/2018')
+			END AS study_age_yrs_SY2
+		,CASE 
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '6/1/2019'), birth_date), '6/1/2019') < 0
+				THEN DATEDIFF(YEAR, birth_date, '6/1/2019') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '6/1/2019')
+			END AS study_age_yrs_SY3
+		,CASE 
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '6/1/2020'), birth_date), '6/1/2020') < 0
+				THEN DATEDIFF(YEAR, birth_date, '6/1/2020') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '6/1/2020')
+			END AS study_age_yrs_SY4	
+		,CASE 
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '6/1/2021'), birth_date), '6/1/2021') < 0
+				THEN DATEDIFF(YEAR, birth_date, '6/1/2021') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '6/1/2021')
+			END AS study_age_yrs_SY5
+		,CASE 
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '6/1/2022'), birth_date), '6/1/2022') < 0
+				THEN DATEDIFF(YEAR, birth_date, '6/1/2022') - 1
+			ELSE DATEDIFF(YEAR, birth_date, '6/1/2022')
+			END AS study_age_yrs_SY6
 	FROM #cohort_demographic
 ) a;
