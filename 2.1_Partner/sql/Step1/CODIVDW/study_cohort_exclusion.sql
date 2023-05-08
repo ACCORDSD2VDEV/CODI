bcb091846d29ba62f65cca074cc890534f4fe4f3
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-05-08 15:55:41 -0600

    Update study_cohort_exclusion.sql
    See commit to cohort.sql for additional changes. The changes to this code included:
    
    •	Line 5, 6, & 7: change from  ‘1/1/2017’ to ‘6/1/2017’
    •	Line 21: change from  change from  ‘1 -Jun-2016’ to ‘1-Jan-2017’
    •	Line 22: change from  change from  ‘1 -Jan-2017’ to ‘1-Jun-2017’

--- 2.1_Partner/sql/Step1/CODIVDW/study_cohort_exclusion.sql
+++ 2.1_Partner/sql/Step1/CODIVDW/study_cohort_exclusion.sql
@@ -5,8 +5,8 @@ FROM (
 	SELECT PERSON_ID AS patid
 		,birth_date
 		,CASE 
-			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '1/1/2017'), birth_date), '1/1/2017') < 0
-				THEN DATEDIFF(YEAR, birth_date, '1/1/2017') - 1
+			WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, birth_date, '6/1/2017'), birth_date), '6/1/2017') < 0
+				THEN DATEDIFF(YEAR, birth_date, '6/1/2017') - 1
 			ELSE DATEDIFF(YEAR, birth_date, '1/1/2017')
 			END AS study_age_yrs
 		,CASE 
@@ -18,8 +18,8 @@ FROM (
 	LEFT JOIN (
 		SELECT DISTINCT @PERSON_ID_PATID patid
 		FROM @SCHEMA.@SESSION
-		WHERE session_date >= '1-Jun-2016'
-			AND session_date < '1-Jan-2017'
+		WHERE session_date >= '1-Jan-2017'
+			AND session_date < '1-Jun-2017'
 			--AND programid IN (
 			--	SELECT programid
 			--	FROM #study_programs
