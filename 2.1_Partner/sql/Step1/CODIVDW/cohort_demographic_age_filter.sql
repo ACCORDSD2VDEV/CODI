66ac893636fbaeb13195cfb94bcaaf70e72a851e
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-05-15 11:26:46 -0600

    Update cohort_demographic_age_filter.sql
    Troubleshooting with Emily Bacon

--- 2.1_Partner/sql/Step1/CODIVDW/cohort_demographic_age_filter.sql
+++ 2.1_Partner/sql/Step1/CODIVDW/cohort_demographic_age_filter.sql
@@ -12,16 +12,13 @@ FROM (
   	hispanic,
   	yr,
   	loc_start --,
-  	--study_age_yrs_2017,
-  	--study_age_yrs_2018,
-  	--study_age_yrs_2019
+  	study_age_yrs_SY1,
+  	study_age_yrs_SY2,
+  	study_age_yrs_SY3
   FROM 
   	#cohort_demographic_age 
   WHERE 
-  	(yr = 'SY1' AND study_age_yrs_2017 BETWEEN 2 AND 19) OR
-  	(yr = 'SY2' AND study_age_yrs_2018 BETWEEN 2 AND 19) OR
-  	(yr = 'SY3' AND study_age_yrs_2019 BETWEEN 2 AND 19) OR 
-    (yr = 'SY4' AND study_age_yrs_2019 BETWEEN 2 AND 19) OR 
-    (yr = 'SY5' AND study_age_yrs_2019 BETWEEN 2 AND 19) OR 
-    (yr = 'SY6' AND study_age_yrs_2019 BETWEEN 2 AND 19) 
+  	(yr = 'SY1' AND study_age_yrs_SY1 BETWEEN 2 AND 19) OR
+  	(yr = 'SY2' AND study_age_yrs_SY2 BETWEEN 2 AND 19) OR
+  	(yr = 'SY3' AND study_age_yrs_SY3 BETWEEN 2 AND 19)
 ) a;
