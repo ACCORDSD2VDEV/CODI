4ac24e76b253c1a757780fc0fef778f2987e18ec
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-05-22 12:15:24 -0600

    Update encounters_vital_join.sql
    Updated timeline from 1/1/17 to 6/1/2017

--- 2.1_Partner/sql/Step3/CODIVDW/encounters_vital_join.sql
+++ 2.1_Partner/sql/Step3/CODIVDW/encounters_vital_join.sql
@@ -17,8 +17,8 @@ FROM (
 	JOIN @SCHEMA.VITAL_SIGNS v ON e.ENC_ID = v.ENC_ID
 	WHERE e.ENCTYPE = 'AV'
 		AND ht IS NOT NULL
-		AND v.measure_date BETWEEN '1/1/2017'
-			AND '12/31/2017'
+		AND v.measure_date BETWEEN '6/1/2017'
+			AND '5/31/2017'
 	) height
 JOIN (
 	SELECT e.ENC_ID
@@ -30,8 +30,8 @@ JOIN (
 	JOIN @SCHEMA.VITAL_SIGNS v ON e.ENC_ID = v.ENC_ID
 	WHERE e.ENCTYPE = 'AV'
 		AND wt IS NOT NULL
-		AND v.measure_date BETWEEN '1/1/2017'
-			AND '12/31/2017'
+		AND v.measure_date BETWEEN '6/1/2017'
+			AND '5/31/2017'
 	) weight ON weight.ENC_ID = height.ENC_ID
 	AND weight.PERSON_ID = height.PERSON_ID
 WHERE ht IS NOT NULL
