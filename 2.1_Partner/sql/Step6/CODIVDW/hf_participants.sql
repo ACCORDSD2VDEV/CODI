3297a11e41f47eae274609e188ec2f129459d0c2
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-05-22 12:35:04 -0600

    Update hf_participants.sql
    Updated timeline from 1/1/17->6/1/17

--- 2.1_Partner/sql/Step6/CODIVDW/hf_participants.sql
+++ 2.1_Partner/sql/Step6/CODIVDW/hf_participants.sql
@@ -10,7 +10,7 @@ FROM @SCHEMA.@SESSION s
 JOIN @SCHEMA.@PROGRAM pr ON pr.programid = s.programid
 JOIN @SCHEMA.@LINK l ON s.patid = l.@PERSON_ID_PATID
 JOIN #patientlist pl ON pl.linkid = l.@LINK_ID_COLUMN_VALUE
-WHERE s.session_date >= '1/1/2017'
+WHERE s.session_date >= '6/1/2017'
 	AND s.programid = 'hf'
 ORDER BY l.@LINK_ID_COLUMN_VALUE
 	,s.session_date DESC
