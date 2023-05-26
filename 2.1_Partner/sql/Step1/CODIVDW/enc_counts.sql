80409dd9540ceac9a4ac76087812e87da934e879
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-05-26 08:36:54 -0600

    Update enc_counts.sql
    Troubleshooting with Emily B and Mark G

--- 2.1_Partner/sql/Step1/CODIVDW/enc_counts.sql
+++ 2.1_Partner/sql/Step1/CODIVDW/enc_counts.sql
@@ -11,11 +11,15 @@ FROM (
   		CASE WHEN  e.ADATE >= '2016-6-1' AND e.ADATE < '2017-6-1' THEN 'SY0'
 			 WHEN e.ADATE >= '2017-6-1' AND e.ADATE < '2018-6-1' THEN 'SY1'
   			 WHEN  e.ADATE >= '2018-6-1' AND e.ADATE < '2019-6-1' THEN 'SY2'
-  			 ELSE 'IGNORE'
+  			 WHEN  e.ADATE >= '2019-6-1' AND e.ADATE < '2020-6-1' THEN 'SY3',
+	  		 WHEN  e.ADATE >= '2020-6-1' AND e.ADATE < '2021-6-1' THEN 'SY4',
+	 		 WHEN  e.ADATE >= '2021-6-1' AND e.ADATE < '2022-6-1' THEN 'SY5',
+	  		 WHEN  e.ADATE >= '2022-6-1' AND e.ADATE < '2023-1-1' THEN 'SY6',
+	  		 ELSE 'IGNORE'
   		END AS yr
   	FROM @SCHEMA.@ENCOUNTERS e
   	JOIN @SCHEMA.@LINK l on l.@PERSON_ID_PATID = e.PERSON_ID
-  	WHERE e.ADATE >= '2017-6-1' AND e.ADATE < '2019-6-1'
+  	WHERE e.ADATE >= '2017-6-1' AND e.ADATE < '2023-1-1'
   ) AS encounter_plus_year
   GROUP BY linkid, patid, yr
 ) a;
