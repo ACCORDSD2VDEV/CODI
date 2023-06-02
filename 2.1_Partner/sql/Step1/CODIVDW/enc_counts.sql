8be72b94b03dde22235d3814508137f036742053
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-06-01 21:38:32 -0600

    Update enc_counts.sql
    Resolving comma issue.

--- 2.1_Partner/sql/Step1/CODIVDW/enc_counts.sql
+++ 2.1_Partner/sql/Step1/CODIVDW/enc_counts.sql
@@ -7,14 +7,16 @@ FROM (
 	,yr
 	,COUNT(ENC_ID) AS encN
   FROM (
-  	SELECT l.@LINKID_COLUMN_VALUE AS linkid, e.PERSON_ID as patid, e.ENC_ID,
-  		CASE WHEN  e.ADATE >= '2016-6-1' AND e.ADATE < '2017-6-1' THEN 'SY0'
+  	SELECT l.@LINKID_COLUMN_VALUE AS linkid
+	  	, e.PERSON_ID as patid
+	  	, e.ENC_ID
+	  	, CASE WHEN  e.ADATE >= '2016-6-1' AND e.ADATE < '2017-6-1' THEN 'SY0'
 			 WHEN e.ADATE >= '2017-6-1' AND e.ADATE < '2018-6-1' THEN 'SY1'
   			 WHEN  e.ADATE >= '2018-6-1' AND e.ADATE < '2019-6-1' THEN 'SY2'
-  			 WHEN  e.ADATE >= '2019-6-1' AND e.ADATE < '2020-6-1' THEN 'SY3',
-	  		 WHEN  e.ADATE >= '2020-6-1' AND e.ADATE < '2021-6-1' THEN 'SY4',
-	 		 WHEN  e.ADATE >= '2021-6-1' AND e.ADATE < '2022-6-1' THEN 'SY5',
-	  		 WHEN  e.ADATE >= '2022-6-1' AND e.ADATE < '2023-1-1' THEN 'SY6',
+  			 WHEN  e.ADATE >= '2019-6-1' AND e.ADATE < '2020-6-1' THEN 'SY3'
+	  		 WHEN  e.ADATE >= '2020-6-1' AND e.ADATE < '2021-6-1' THEN 'SY4'
+	 		 WHEN  e.ADATE >= '2021-6-1' AND e.ADATE < '2022-6-1' THEN 'SY5'
+	  		 WHEN  e.ADATE >= '2022-6-1' AND e.ADATE < '2023-1-1' THEN 'SY6'
 	  		 ELSE 'IGNORE'
   		END AS yr
   	FROM @SCHEMA.@ENCOUNTERS e
