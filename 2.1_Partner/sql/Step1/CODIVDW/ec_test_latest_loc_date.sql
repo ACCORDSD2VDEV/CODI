42058d0674cea0aa55671f62c58bc802db0e5dd6
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-07-31 10:03:57 -0600

    Update ec_test_latest_loc_date.sql
    Error identified by Emily Bacon during test queries

--- 2.1_Partner/sql/Step1/CODIVDW/ec_test_latest_loc_date.sql
+++ 2.1_Partner/sql/Step1/CODIVDW/ec_test_latest_loc_date.sql
@@ -4,7 +4,15 @@ SELECT *
 INTO #ec_test_latest_loc_date
 FROM (
 	SELECT patid
-		,yr
+		,CASE WHEN  yr >= '2016-6-1' AND yr < '2017-6-1' THEN 'SY0'
+			 WHEN yr >= '2017-6-1' AND yr < '2018-6-1' THEN 'SY1'
+  			 WHEN  yr >= '2018-6-1' AND yr < '2019-6-1' THEN 'SY2'
+  			 WHEN  yr >= '2019-6-1' AND yr < '2020-6-1' THEN 'SY3'
+	  		 WHEN  yr >= '2020-6-1' AND yr < '2021-6-1' THEN 'SY4'
+	 		 WHEN  yr >= '2021-6-1' AND yr < '2022-6-1' THEN 'SY5'
+	  		 WHEN  yr >= '2022-6-1' AND yr < '2023-1-1' THEN 'SY6'
+	  		 ELSE 'IGNORE'
+  		END AS yr
 		,MAX(CONVERT(DATE, loc_start)) AS latest_loc_date
 	FROM #ec_test
 	--WHERE loc_start <= CONVERT(DATETIME, '12-31-' + CAST(yr AS VARCHAR(4)))
