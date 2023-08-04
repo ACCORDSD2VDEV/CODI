35067c40d6ad4b81188238b01030e61d83476b34
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-08-04 14:46:45 -0600

    Update cohort_demographic_age_filter.sql
    Troubleshooting with Emily Bacon. Removed:
    
     OR
        (yr = 'SY4' AND study_age_yrs_SY4 BETWEEN 2 AND 19) OR
        (yr = 'SY5' AND study_age_yrs_SY5 BETWEEN 2 AND 19) OR
        (yr = 'SY6' AND study_age_yrs_SY6 BETWEEN 2 AND 19)

--- 2.1_Partner/sql/Step1/CODIVDW/cohort_demographic_age_filter.sql
+++ 2.1_Partner/sql/Step1/CODIVDW/cohort_demographic_age_filter.sql
@@ -20,8 +20,5 @@ FROM (
   WHERE 
   	(yr = 'SY1' AND study_age_yrs_SY1 BETWEEN 2 AND 19) OR
   	(yr = 'SY2' AND study_age_yrs_SY2 BETWEEN 2 AND 19) OR
-  	(yr = 'SY3' AND study_age_yrs_SY3 BETWEEN 2 AND 19) OR
-    (yr = 'SY4' AND study_age_yrs_SY4 BETWEEN 2 AND 19) OR
-    (yr = 'SY5' AND study_age_yrs_SY5 BETWEEN 2 AND 19) OR
-    (yr = 'SY6' AND study_age_yrs_SY6 BETWEEN 2 AND 19) 
+  	(yr = 'SY3' AND study_age_yrs_SY3 BETWEEN 2 AND 19)
 ) a;
