45f12f9ecdfb9047306765de114e289c74287033
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-10-06 08:53:27 -0600

    Update cohort.sql
    ambiguous birth date

--- 2.1_Partner/sql/Step3/CODIVDW/cohort.sql
+++ 2.1_Partner/sql/Step3/CODIVDW/cohort.sql
@@ -25,7 +25,7 @@ SELECT p.linkid, d.PERSON_ID, GENDER, PRIMARY_LANGUAGE, RACE1, hispanic,
 	   CASE WHEN UPPER(p.site) = UPPER(p.index_site) THEN 'T' ELSE 'F' END AS index_site_flag,
 	   CASE 
 		WHEN DATEDIFF(day, DATEADD(year, DATEDIFF(YEAR, d.birth_date, '6/1/2016'), d.birth_date), '6/1/2016') < 0
-			THEN DATEDIFF(YEAR, birth_date, '6/1/2016') - 1
+			THEN DATEDIFF(YEAR, d.birth_date, '6/1/2016') - 1
 		ELSE DATEDIFF(YEAR, d.birth_date, '6/1/2016')
 		END,
 	   inclusion,
