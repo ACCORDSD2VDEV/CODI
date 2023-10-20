bc1344a61146bdeecc836373c21a0a459a92fc55
Ken Scott <9954398+k-scott@users.noreply.github.com>
2023-10-20 15:33:33 -0600

    Update cohort.sql
    Kevin Matthews fixed this bug.

--- 2.1_Partner/sql/Step3/CODIVDW/cohort.sql
+++ 2.1_Partner/sql/Step3/CODIVDW/cohort.sql
@@ -35,4 +35,4 @@ JOIN @SCHEMA.LINK l ON l.@PERSON_ID_PATID = d.PERSON_ID
 JOIN #patientlist p ON p.linkid = l.@LINKID_COLUMN_VALUE
 LEFT OUTER JOIN #study_cohort s ON d.PERSON_ID = s.patid
 --WHERE exclusion != 1;
-WHERE cast(exclusion as int) != 1;
+WHERE cast(exclusion as int) != 1 or exclusion is null;
